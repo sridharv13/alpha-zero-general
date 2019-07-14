c04c12d389185a1108b9edb5095209bced1aa8ce
sridharv13 <sridharv13@gmail.com>
2019-07-14 16:15:49 +0530

    changes

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-
-drive = GoogleDrive(gauth)`
+from google.colab import drive
+drive.mount('/content/drive')
